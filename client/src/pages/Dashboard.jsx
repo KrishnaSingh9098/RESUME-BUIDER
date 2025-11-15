3758c325b347588fc5f9d35d0d6267dd4d89f3e4
KrishnaSingh9098 <arushsingh5103@gmail.com>
2025-11-15 17:35:10 +0530

    NEW

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { Plus, PlusIcon, UploadCloudIcon } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
+import { dummyResumeData } from '../assets/assets'
 
 const Dashboard = () => {
+const colors = ["#9333ea", "#d97706", "#dc2626", "#0284c7", "#16a34a"];
+  const [allResume, setAllResume] = useState([])
+  const loadAllResumes = async () => {
+    setAllResume(dummyResumeData)
+  }
+
+  useEffect(()=>{
+    loadAllResumes()
+  },[])
   return (
     <div>
-      <h1>Dashboard</h1>
+     <div className='max-w-7xl mx-auto px-4 py-8'>
+          <p className='text-2xl font-medium mb-6 bg-gradient-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden'>Welcome, John Doe</p>
+          <div className='flex gap-4'>
+            <button className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-indigo-500 hover:shadow-lg transition-all duration-300 cursor-pointer'>
+              <PlusIcon className='size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-indigo-300 to-indigo-500 text-white rounded-full'/>
+              <p className='text-sm group-hover:text-indigo-600 transition-all'>Create Resume</p>
+            </button>
+             <button className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-purple-500 hover:shadow-lg transition-all duration-300 cursor-pointer'>
+              <UploadCloudIcon className='size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-purple-300 to-purple-500 text-white rounded-full'/>
+              <p className='text-sm group-hover:text-purple-600 transition-all'>Upload Existing</p>
+            </button>
+          </div>
+          <hr className='border-slate-300 my-6 sm:w-[305px]' />
+          <div className='grid grid-cols-2 sm:flex flex-wrap gap-4'>
+    {allResume.map((resume,index)=>{
+    const baseColor = colors[index % colors.length];
+    return(
+      <button key={index} className='relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 border group hover:shadow-lg transition-all duration-300 cursor-pointer'>
+
+      </button>
+    )
+    })}
+          </div>
+     </div>
     </div>
   )
 }
